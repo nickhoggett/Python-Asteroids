1db66485211aa9206c4d6d7bfe934c68929116c3
nick.hoggett@gmail.com <nick.hoggett@gmail.com>
2024-08-20 16:28:17 +0100

    intial setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python-Asteroids
 A classic game of Asteroids
+
+REQUIRED -- pygame==2.6.0
+https://www.pygame.org/news
+
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constants import *
+
+print("Starting asteroids!")
+print("Screen width:",SCREEN_WIDTH)
+print("Screen height:",SCREEN_HEIGHT)
+
+if __name__  == "__main__" :
+    main()
